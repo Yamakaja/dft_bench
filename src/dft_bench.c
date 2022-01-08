15f8a85dd0ea4db5c123301e3718693686ffb4e4
David Winter <dastw@gmx.net>
2022-01-08 21:59:25 +0100

    Add dft23_2 without precomputed coefficients
    Results? Terrible :)

--- src/dft_bench.c
+++ src/dft_bench.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include <immintrin.h>
 
@@ -38,7 +39,7 @@ void prepare_sample_sequence(complex float *buf, size_t len) {
     float t = 0;
 
     for (size_t i = 0; i < len; i++) {
-        buf[i] = sinf(2 * M_PI * SINE_FREQ * t) + NOISE_AMPLITUDE * gaussian(xoroshiro128plus_next(&xoro_state));
+        buf[i] = sinf(2 * M_PI * SINE_FREQ * t); //+ NOISE_AMPLITUDE * gaussian(xoroshiro128plus_next(&xoro_state));
         t += 1.0f/SAMPLE_RATE;
     }
 }
@@ -129,6 +130,40 @@ void dft23(complex float *buf, complex float *coeffs, complex float *out) {
     }
 }
 
+void dft23_2(complex float *buf, complex float *out) {
+    // np.exp(-1j * np.arange(23)/23 * 2*np.pi)
+    const complex float k_rotators[DFT_SIZE+1] =
+      { 1.f       +0.f          * I,  0.96291729f-0.26979677f * I,
+        0.85441940f-0.51958395f * I,  0.68255314f-0.73083596f * I,
+        0.46006504f-0.88788522f * I,  0.20345601f-0.97908409f * I,
+       -0.06824241f-0.99766877f * I, -0.33487961f-0.94226092f * I,
+       -0.57668032f-0.81696989f * I, -0.77571129f-0.63108794f * I,
+       -0.91721130f-0.39840109f * I, -0.99068595f-0.13616665f * I,
+       -0.99068595f+0.13616665f * I, -0.91721130f+0.39840109f * I,
+       -0.77571129f+0.63108794f * I, -0.57668032f+0.81696989f * I,
+       -0.33487961f+0.94226092f * I, -0.06824241f+0.99766877f * I,
+        0.20345601f+0.97908409f * I,  0.46006504f+0.88788522f * I,
+        0.68255314f+0.73083596f * I,  0.85441940f+0.51958395f * I,
+        0.96291729f+0.26979677f * I,  0};
+
+    for (int k = 0; k < DFT_SIZE; k += 4) {
+        __m256 coeff = _mm256_set_ps(0, 1, 0, 1, 0, 1, 0, 1);
+
+        __m256 rotators = _mm256_loadu_ps((float *) &k_rotators[k]);
+
+        __m256 sum = _mm256_set1_ps(0);
+        for (int n = 0; n < DFT_SIZE; n++) {
+            __m256 sample = _mm256_maskload_ps((float *) &buf[n], _mm256_set_epi32(0, 0, 0, 0, 0, 0, -1, -1));
+            sample = _mm256_permutevar8x32_ps(sample, _mm256_set_epi32(1, 0, 1, 0, 1, 0, 1, 0));
+
+            sum = _mm256_add_ps(sum, dft23_cmult4(coeff, sample));
+            coeff = dft23_cmult4(coeff, rotators);
+        }
+
+        _mm256_store_ps((float *) &out[k], sum);
+    }
+}
+
 void gen_dft23_coeffs(complex float *coeffs) {
     for (int k = 0; k < DFT_SIZE; k++) {
         for (int n = 0; n < DFT_SIZE; n++)
@@ -137,7 +172,7 @@ void gen_dft23_coeffs(complex float *coeffs) {
     }
 }
 
-int64_t bench_dft23(complex float *buf, size_t len) {
+int64_t bench_dft23(complex float *buf, size_t len, bool second) {
     struct timespec tp_start; // tv_sec, tv_nsec
     struct timespec tp_end; // tv_sec, tv_nsec
 
@@ -153,7 +188,10 @@ int64_t bench_dft23(complex float *buf, size_t len) {
     clock_gettime(CLOCK_MONOTONIC, &tp_start);
 
     for (ssize_t i = 0; i < ((ssize_t) (len / DFT_SIZE)); i++) {
-        dft23(&buf[DFT_SIZE*i], coeffs, out);
+        if (second)
+            dft23_2(&buf[DFT_SIZE*i], out);
+        else
+            dft23(&buf[DFT_SIZE*i], coeffs, out);
 
         for (int j = 0; j < DFT_SIZE; j++)
             sum += out[j];
@@ -184,7 +222,7 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    int which = 3;
+    int which = 0xFF;
     if (argc > 2 && sscanf(argv[2], "%d", &which) != 1) {
         fprintf(stderr, "%s: Invalid selector id: %s\n", argv[0], argv[2]);
         return EXIT_FAILURE;
@@ -221,7 +259,12 @@ int main(int argc, char *argv[]) {
     }
 
     if (which & 0x2) {
-        duration = bench_dft23(samples, len);
+        duration = bench_dft23(samples, len, false);
+        printf("DFT:    Duration: %ld ns, Throughput: %f MS/s\n", duration, len / ((double)duration) * 1e3);
+    }
+
+    if (which & 0x4) {
+        duration = bench_dft23(samples, len, true);
         printf("DFT:    Duration: %ld ns, Throughput: %f MS/s\n", duration, len / ((double)duration) * 1e3);
     }
 
